4461082bdcc57613111b63b03dd410761c551349
Sebastien Luk <55606361+sebluk@users.noreply.github.com>
2022-02-23 13:31:56 -0500

    Update employerview.service.ts
    updated url database

--- Frontend/src/app/employerview/employerview.service.ts
+++ Frontend/src/app/employerview/employerview.service.ts
@@ -18,7 +18,7 @@ export class EmployerService {
 
     this.http
       .get<any>(
-        'http://epayroll-data-eda-epayroll-poc.apps.xnkpeyx0.canadacentral.aroapp.io/epayroll/processed',
+        'http://epayroll-data-db-eda-epayroll-poc3.apps.xnkpeyx0.canadacentral.aroapp.io/epayroll/processed',
         { headers }
       )
       .subscribe({
@@ -31,7 +31,7 @@ export class EmployerService {
       });
 
     return this.http.get<any>(
-      'http://epayroll-data-eda-epayroll-poc.apps.xnkpeyx0.canadacentral.aroapp.io/epayroll/processed',
+      'http://epayroll-data-db-eda-epayroll-poc3.apps.xnkpeyx0.canadacentral.aroapp.io/epayroll/processed',
       { headers }
     );
   }
